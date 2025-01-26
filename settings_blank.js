74a7a8a97f90fb4eac8d889cbb524b71b618c204
KayPure <43040621+KayPure@users.noreply.github.com>
2025-01-26 22:56:01 +0100

    Update settings_blank.js

--- settings_blank.js
+++ settings_blank.js
@@ -1,18 +1,14 @@
 // Streamlabs Socket Token
-exports.token = 'your streamlabs api token';
-// get ur socket api token from https://streamlabs.com/dashboard#/settings/api-settings
+exports.token = 'your streamlabs api token'; // get ur socket api token from https://streamlabs.com/dashboard#/settings/api-settings
 
 // Connection to Server and Channel
 exports.cluster = "aws";
 exports.reconnect = "true";
-exports.channels = ["twitch channel to join"];
+exports.channels = ["Enter Channel"]; // Twitch channel to join
 
 // Username / Password
-exports.username = "bot name";
-exports.password = "oauth:your bots oauth";
-// make sure u are connected with your bots account on twitch, NOT your main account!
-// you can get your oauth on https://twitchapps.com/tmi/
+exports.username = "Username(bot)"; // Bot username. Dont use a mainaccount because the script will move the mouse and trigger keys. Instead, use an account which you dont use to chat e.g!
+exports.password = "bots oauth"; // Generate a oauth for your bot. You can get your oauth on https://twitchtokengenerator.com/ 
 
+// !!! after editing this file, rename it to "settings.js" !!! \\
 
-
-// !!! after editing this file, rename it to "settings.js !!!"
